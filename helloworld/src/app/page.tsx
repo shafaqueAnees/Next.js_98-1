08bc55e50e49e48ab8da25a53b3c91311d123b68
shafq <julletshaikh@gmail.com>
2024-09-14 00:18:39 +0500

    shafaq

--- helloworld/src/app/page.tsx
+++ helloworld/src/app/page.tsx
@@ -4,11 +4,6 @@ import Sidebar from "./components/sidebar";
 
 export default function Home() {
   return (
-    <div>
-      <Navbar/>
-          <h1>hellow world {1+1}</h1>
-          <Sidebar/>
-         
-        </div>
-  );
+    <div>Hello from Shafaque Anees</div>
+  )
 }
