dd29698e52966c6dcea70131969f55c12bbde472
shafq <julletshaikh@gmail.com>
2024-10-17 23:57:05 +0500

    done by shafq

--- helloworld/src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import Image from "next/image";
-import Navbar from "./components/navbaar";
-import Sidebar from "./components/sidebar";
-
-export default function Home() {
-  return (
-    <div>Hello from Shafaque Anees</div>
-  )
-}
